fcb368dff9623edf3f3b0f611502808a06e042dc
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2022-12-29 12:36:08 -0800

    add strapi made mock data

---
+++ src/utils/mockStrapiResponses.ts
@@ -0,0 +1,118 @@
+export const header = { 
+  "data": { 
+    "id": 1, 
+    "attributes": {
+      "title": "Logan Roth", 
+      "createdAt": "2022-12-29T18:43:58.158Z", 
+      "updatedAt": "2022-12-29T18:51:36.035Z", 
+      "publishedAt": "2022-12-29T18:51:36.032Z" } 
+    }, 
+  "meta": {} 
+}
+
+export const footer = {
+  "data": {
+    "id":1,
+    "attributes": {
+      "linkedinURL":"https://www.linkedin.com/in/lo-gr/",
+      "email":"logan.roth.g@gmail.com",
+      "githubURL":"https://github.com/Lo-GR",
+      "createdAt":"2022-12-29T20:10:07.069Z",
+      "updatedAt":"2022-12-29T20:10:09.475Z",
+      "publishedAt":"2022-12-29T20:10:09.471Z"}
+    },
+  "meta":{}
+}
+
+export const home = {
+  "data":{
+    "id":1,
+    "attributes":{
+      "createdAt": "2022-12-29T18:50:40.455Z",
+      "updatedAt": "2022-12-29T18:50:48.909Z",
+      "publishedAt": "2022-12-29T18:50:48.905Z",
+      "widget1":{
+        "id":1,
+        "hello":
+        "Hello, I'm Logan!",
+        "subtext": "idk can't think rn"},
+      "widget2":{
+        "id":1,
+        "title":"Front-end",
+        "subtitle":"- Developer\n- Engineer\n- Enjoyer"}
+        ,
+      "widget3":{
+        "id":1,
+        "linkedinURL":"https://www.linkedin.com/in/lo-gr/"},
+      "widget4":{
+        "id":1,
+        "skills":{
+          "skills":["skill","skill","skill","skill","skill"]}
+        }
+      }
+    },
+  "meta":{}
+}
+
+export const projectPage = {
+"data": {
+  "id":1,
+  "attributes": {
+    "createdAt":"2022-12-29T20:20:52.300Z",
+    "updatedAt":"2022-12-29T20:20:53.362Z",
+    "publishedAt":"2022-12-29T20:20:53.358Z",
+    "project1": {
+      "id":1,
+      "image": "Insert Image Link Here",
+      "title":"Project Title 1",
+      "description":"Project Description 1",
+      "tech":"Unity, lol" 
+    },
+      "project2": {
+        "id":2,
+        "image": "Insert Image Link Here",
+        "title":"Project Title 2",
+        "description":"Project Description 2",
+        "tech":"Unity, lol"
+      },
+      "project3": {
+        "id":3,
+        "image": "Insert Image Link Here",
+        "title":"Project Title 3",
+        "description":"Project Description 3",
+        "tech":"Unity, lol"
+      }
+    }
+  },
+  "meta":{}
+}
+
+export const background = {
+  "data": {
+    "id":1,
+    "attributes": {
+      "image":"insert image link here",
+      "skills":"Markdown text here",
+      "experience":"Markdown text here",
+      "createdAt":"2022-12-29T20:31:06.055Z",
+      "updatedAt":"2022-12-29T20:31:07.428Z",
+      "publishedAt":"2022-12-29T20:31:07.424Z"
+    }
+  },
+  "meta":{}
+}
+
+export const about = {
+  "data": {
+    "id":1,
+    "attributes": {
+      "design":"Background on design, mention Brandon Lemus design here to give credit",
+      "tech":"Tech Stack",
+      "goals":"Goals and underlying tech reasoning here (CMS ready, etc)",
+      "createdAt":"2022-12-29T20:34:17.999Z",
+      "updatedAt":"2022-12-29T20:34:19.568Z",
+      "publishedAt":"2022-12-29T20:34:19.564Z"
+    }
+  },
+  "meta":{}
+}
