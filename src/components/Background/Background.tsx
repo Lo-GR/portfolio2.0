20c85f2d77d699a02513f191152755e2aba585d7
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2023-02-18 15:41:10 -0800

    hide scrollbar

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -10,7 +10,7 @@ const Background = () => {
   const portraitProps = background.data.attributes.image;
 
   return (
-    <div className={classNames(`flex flex-1 basis-3/5 flex-wrap gap-5 p-3 md:p-10 bg-dark-500 justify-center overflow-y-scroll`)}>
+    <div className={classNames(`flex flex-1 basis-3/5 flex-wrap gap-5 p-3 md:p-10 bg-dark-500 justify-center overflow-y-scroll scrollbar-hide`)}>
       <Portrait className="flex shrink-1 grow-1 basis-full md:basis-[35%]" image={portraitProps} />
       <BackgroundSkills className="flex shrink-1 grow-1 basis-full md:basis-[55%]" skills={backgroundSkillsProps} />
       <Blurb className="flex shrink-1 grow-1 basis-full md:basis-[calc(90%+20px)]" experience={blurbProps} />
