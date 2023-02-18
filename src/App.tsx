a05f2628f3e62bde06d4707e26565c651d7539a6
Zizzs <Zizzs17@gmail.com>
2023-02-18 11:45:19 -0800

    Stuff.

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,7 @@ import About from './components/About/About';
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
-  const footerHeight: number = globalConfig.footer.height;
-  const headerHeight: number = globalConfig.header.height;
-  const contentHeight: string = getHeight(footerHeight + headerHeight);
-
   const [open, setOpen] = useState(false);
-  const [heightStyling, setHeightStyling] = useState(contentHeight);
-
-  const location = window.location.pathname;
-  // let heightStyling = contentHeight;
-
-  useEffect(() => {
-    if (location === '/background') {
-      setHeightStyling('h-full')
-    }
-  }, [location])
 
   const handleMobileNav = () => {
     setOpen(!open);
@@ -39,16 +25,16 @@ function App() {
   return (
     <div className='md:h-full'>
       <Header open={open} handleOpen={handleMobileNav} />
-      <div className={classNames(heightStyling, 'flex flex-row relative')}>
+      <div className={classNames('flex flex-row relative h-full')}>
         <BrowserRouter>
           <Nav />
-          <MobileNav open={open} handleOpen={handleMobileNav} />
           <Routes>
             <Route index path="/" element={<Home />} />
             <Route index path="/projects" element={<Projects />} />
             <Route index path="/background" element={<Background />} />
             <Route index path="/about" element={<About />} />
           </Routes>
+          <MobileNav open={open} handleOpen={handleMobileNav} />
         </BrowserRouter>
       </div>
       <Footer linkedinURL="https://www.linkedin.com/in/lo-gr/" email="logan.roth.g@gmail.com" githubURL="https://github.com/Lo-GR" />
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 const About = () => {
   return (
-    <div className={classNames(`flex flex-1 basis-3/5 h-full flex-wrap gap-5 p-3 md:p-10 bg-dark-500 justify-center`)}>
+    <div className={classNames(`flex flex-1 basis-3/5 h-full flex-wrap gap-5 p-3 md:p-10 bg-dark-500 justify-center min-h-[100vh]`)}>
       <div className="flex shrink-1 grow-1 basis-full md:basis-[55%] bg-white" />
       <div className="flex shrink-1 grow-1 basis-full md:basis-[35%] bg-white" />
       <div className="flex shrink-1 grow-1 basis-full md:basis-[calc(90%+20px)] bg-white" />
