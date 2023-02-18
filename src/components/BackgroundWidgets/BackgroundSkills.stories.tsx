e2f1538f2cfc481c7c4b06ea9aa2a88976c77861
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2023-02-17 19:15:39 -0800

    add backgroundskills widget to background

---
+++ src/components/BackgroundWidgets/BackgroundSkills.stories.tsx
@@ -0,0 +1,8 @@
+import BackgroundSkills from "./BackgroundSkills";
+
+export default {
+  title: 'Background/BackgroundSKills',
+  component: BackgroundSkills
+}
+
+export const Background = () => <BackgroundSkills skills={["skills", "skills", "skills", "skills", "skills", "skills", "skills", "skills"]} className="" />
---
+++ src/components/BackgroundWidgets/BackgroundSkills.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames';
+
+interface BackgroundSkillsProps {
+  skills: string[]
+  className?: string;
+}
+
+export default function BackgroundSkills({ skills, className }: BackgroundSkillsProps) {
+  return (
+    <div className={classNames(className, 'bg-dark-400 w-full p-10 rounded')}>
+      <ul className="list-disc flex flex-col flex-wrap w-full h-full text-light-500">
+        {skills.map(skill => <li>{skill}</li>)}
+      </ul>
+    </div>
+  )
+}
